a695096abdd12453319bcfd7c9c1ba83b999748a
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-01 18:35:12 +0530

    Removed the Boilerplate. Added the Boards Controller

---
+++ src/BoardGamesListAPI/Controllers/BoardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoardGamesListAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardController : ControllerBase
+    {
+        public ActionResult Get()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
