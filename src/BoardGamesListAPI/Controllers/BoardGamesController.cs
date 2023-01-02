88b43ee6fc30e22e14a7e884dbb0c1583ebcddf3
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-02 16:34:35 +0530

    Added Filtering and paging

--- src/BoardGamesListAPI/Controllers/BoardGamesController.cs
+++ src/BoardGamesListAPI/Controllers/BoardGamesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BoardGamesListAPI.Domain;
+using Marten;
+using Marten.Pagination;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BoardGamesListAPI.Controllers
@@ -7,10 +10,40 @@ namespace BoardGamesListAPI.Controllers
     [ApiController]
     public class BoardGamesController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult Get()
+        private readonly IDocumentStore documentStore;
+
+        public BoardGamesController(IDocumentStore documentStore)
         {
-            return Ok("Hello World");
+            this.documentStore = documentStore;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<RestDTO<List<BoardGames>>>> Get(
+            int pageIndex = 1
+            , int pagesize = 10
+            , string sortColumn = "Name",
+             string sortOrder = "ASC")
+        {
+            using (var session = documentStore.LightweightSession())
+            {
+                var query = session.Query<BoardGames>();
+                if (sortOrder == "ASC")
+                    query.OrderBy(p => p.Name);
+                else
+                {
+                    query.OrderByDescending(p => p.Name);
+                }
+                var dbResult= await query.ToPagedListAsync(pageIndex,pagesize);
+
+                var responsedata = new RestDTO<List<BoardGames>>
+                {
+                    Data = dbResult.ToList(),
+                    PageIndex = dbResult.PageNumber,
+                    PageSize = dbResult.PageSize,
+                    TotalCount = dbResult.TotalItemCount
+                };
+                return Ok(responsedata);
+            }
         }
     }
 }
---
+++ src/BoardGamesListAPI/Controllers/RestDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BoardGamesListAPI.Controllers
+{
+    public class RestDTO<T>
+    {
+        public T Data { get; set; }
+
+        public long PageSize { get; set; }
+
+        public long PageIndex { get; set; } = 0;
+
+        public long TotalCount { get; set; }
+    }
+}
